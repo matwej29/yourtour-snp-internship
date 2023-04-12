ae6e6fed3904fa9c213d351e471d9779403f8fd0
matwej29 <matweej2004@yandex.ru>
2023-04-12 17:58:06 +0300

    migration to nextjs

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/main.css";
+import Menu from "./components/menu/Menu.jsx";
+
+// main page
+const IndexPage = () => (
+  <>
+    <Menu />
+  </>
+);
+
+export default IndexPage;
