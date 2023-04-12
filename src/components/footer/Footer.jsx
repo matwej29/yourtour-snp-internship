b79d6a752a97850290989b1e8dc1f65086acaefd
matwej29 <matweej2004@yandex.ru>
2023-04-12 17:59:52 +0300

    update footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
+import FooterList from './__list/FooterList';
+import FooterListItem from './__list-item/FooterListItem';
+import FooterLink from './__link/FooterLink';
+
+import VkontakteLogo from '../icons/vkontakte/VkontakteLogo';
+import FacebookLogo from '../icons/facebook/FacebookLogo';
+import InstagramLogo from '../icons/instagram/InstagramLogo';
+
 import './footer.css';
 
-const Footer = () => {
+const Footer = () => (
+    <footer className="footer footer-container container">
+        <p className="p_n p_gray p_clear-margin">Наши социальные сети</p>
+        <FooterList>
+            <FooterListItem>
+                <InstagramLogo />
+                <FooterLink href="#">instagram</FooterLink>
+            </FooterListItem>
+            <FooterListItem>
+                <FacebookLogo />
+                <FooterLink href="#">facebook</FooterLink>
+            </FooterListItem>
+            <FooterListItem>
+                <VkontakteLogo />
+                <FooterLink href="#">vkontakte</FooterLink>
+            </FooterListItem>
+        </FooterList>
 
-}
+    </footer>
+);
+
+export default Footer;
