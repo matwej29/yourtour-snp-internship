0ff03d0281677d594a5e71aadc2c3184b22bdb76
matwej29 <matweej2004@yandex.ru>
2023-09-11 19:04:48 +0300

    add component button

--- components/button/Button.jsx
+++ components/button/Button.jsx
@@ -1,28 +1,35 @@
 import React from "react";
-import "./button.css";
-import './_type/button_type_details.css';
-import './_type/button_type_find.css';
-import './_type/button_type_reset.css';
-import './_type/button_type_tour.css';
 
-export const ButtonDetails = ({ children, onClick }) => (
-  <button onClick={onClick} className={'button button_type_details'}>{children}</button>
+export const ButtonDetails = ({ children, onClick, className = "" }) => (
+  <button
+    onClick={onClick}
+    className={`button button_type_details ${className}`}>
+    {children}
+  </button>
 );
 
-export const ButtonFind = ({ children, onClick }) => (
-  <button onClick={onClick} className={'button button_type_find'}>{children}</button>
+export const ButtonFind = ({ children, onClick, className = "" }) => (
+  <button onClick={onClick} className={`button button_type_find ${className}`}>
+    {children}
+  </button>
 );
 
-export const ButtonReset = ({ children, onClick }) => (
-  <button onClick={onClick} className={'button button_type_reset'}>{children}</button>
+export const ButtonReset = ({ children, onClick, className = "" }) => (
+  <button onClick={onClick} className={`button button_type_reset ${className}`}>
+    {children}
+  </button>
 );
 
-export const ButtonTour = ({ children, onClick }) => (
-  <button onClick={onClick} className={'button button_type_tour'}>{children}</button>
+export const ButtonTour = ({ children, onClick, className = "" }) => (
+  <button onClick={onClick} className={`button button_type_tour ${className}`}>
+    {children}
+  </button>
 );
 
-const Button = ({ children, onClick }) => (
-  <button onClick={onClick} className={'button'}>{children}</button>
+const Button = ({ children, onClick, className = "" }) => (
+  <button onClick={onClick} className={`button ${className}`}>
+    {children}
+  </button>
 );
 
 export default Button;
