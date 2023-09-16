2891a7fe4c05cce94c7408f3676f33e1de604f83
matwej29 <matweej2004@yandex.ru>
2023-09-16 18:00:53 +0300

    add NavigationTab

---
+++ components/NavigationTab/NavigationTab.jsx
@@ -0,0 +1,16 @@
+import "react";
+
+import Tab from "../tab/Tab";
+import Button from "../button/Button";
+
+const NavigationTab = () => (
+  <Tab>
+    <Button className="p_b" choosed={true}>Популярные</Button>
+    <Button className="p_b">Авторские</Button>
+    <Button className="p_b">Походы</Button>
+    <Button className="p_b">Сплавы</Button>
+    <Button className="p_b">Велопрогулки</Button>
+  </Tab>
+);
+
+export default NavigationTab;
