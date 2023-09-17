bda11cd2f9fb147374ded277d0b42620c1a52478
matwej29 <matweej2004@yandex.ru>
2023-09-17 09:00:59 +0300

    add headings

---
+++ components/headings/Headings.jsx
@@ -0,0 +1,5 @@
+export const H1 = ({ children, className = '' }) => <h1 className={`h1 ${className}`}>{children}</h1>;
+
+export const H2 = ({ children, className = '' }) => <h2 className={`h2 ${className}`}>{children}</h2>;
+
+export const H3 = ({ children, className = '' }) => <h3 className={`h3 ${className}`}>{children}</h3>;
