c87a23cd2b40af8330dce40f44ae253c546ba3b6
matwej29 <matweej2004@yandex.ru>
2023-09-17 11:27:11 +0300

    add photoBlock title

--- components/PhotoBlock/PhotoBlock.jsx
+++ components/PhotoBlock/PhotoBlock.jsx
@@ -1,9 +1,21 @@
 import PhotoSection from "../photo/Photo";
-import MainBlock from "../main-block/MainBlock";
+import MainBlock, {
+  MainBlock__title,
+  MainBlock__description,
+} from "../main-block/MainBlock";
+import { H2 } from "../headings/Headings";
 
 // TODO: возможно, стоит перенести сюда содержимое PhotoSection.jsx
 const PhotoBlock = () => (
   <MainBlock container={false} id="photo">
+    <MainBlock__title>
+      <H2>Фотографии путешествий</H2>
+      <MainBlock__description>
+        Идейные соображения высшего порядка, <br />а также рамки и место
+        обучения кадров
+      </MainBlock__description>
+    </MainBlock__title>
+
     <PhotoSection />
   </MainBlock>
 );
