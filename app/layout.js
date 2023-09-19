56376b8af2f8424b72e47272e5ea5cef3f69a8f5
matwej29 <matweej2004@yandex.ru>
2023-09-19 08:32:57 +0300

    add page and layout

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,63 @@ const Roboto = localFont({
 export default function RootLayout({ children }) {
   return (
     <html lang="ru" className={Roboto.className}>
+      <title>YourTour</title>
+      <head>
+        <link
+          type="image/png"
+          sizes="16x16"
+          rel="icon"
+          href="/icons/icons8-around-the-globe-pastel-16.png"
+        />
+        <link
+          type="image/png"
+          sizes="32x32"
+          rel="icon"
+          href="/icons/icons8-around-the-globe-pastel-32.png"
+        />
+        <link
+          type="image/png"
+          sizes="96x96"
+          rel="icon"
+          href="/icons/icons8-around-the-globe-pastel-96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="72x72"
+          href="/icons/icons8-around-the-globe-pastel-72.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/icons/icons8-around-the-globe-pastel-96.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          type="image/png"
+          sizes="57x57"
+          href="/icons/icons8-around-the-globe-pastel-57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          type="image/png"
+          sizes="60x60"
+          href="/icons/icons8-around-the-globe-pastel-60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          type="image/png"
+          sizes="72x72"
+          href="/icons/icons8-around-the-globe-pastel-72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          type="image/png"
+          sizes="76x76"
+          href="/icons/icons8-around-the-globe-pastel-76.png"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -14,63 +14,6 @@ import HistoriesBlock from "../components/HistoriesBlock/HistoriesBlock.jsx";
 const IndexPage = () => {
   return (
     <>
-      <title>YourTour</title>
-      <Head>
-        <link
-          type="image/png"
-          sizes="16x16"
-          rel="icon"
-          href="/icons/icons8-around-the-globe-pastel-16.png"
-        />
-        <link
-          type="image/png"
-          sizes="32x32"
-          rel="icon"
-          href="/icons/icons8-around-the-globe-pastel-32.png"
-        />
-        <link
-          type="image/png"
-          sizes="96x96"
-          rel="icon"
-          href="/icons/icons8-around-the-globe-pastel-96.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="72x72"
-          href="/icons/icons8-around-the-globe-pastel-72.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="96x96"
-          href="/icons/icons8-around-the-globe-pastel-96.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          type="image/png"
-          sizes="57x57"
-          href="/icons/icons8-around-the-globe-pastel-57.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          type="image/png"
-          sizes="60x60"
-          href="/icons/icons8-around-the-globe-pastel-60.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          type="image/png"
-          sizes="72x72"
-          href="/icons/icons8-around-the-globe-pastel-72.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          type="image/png"
-          sizes="76x76"
-          href="/icons/icons8-around-the-globe-pastel-76.png"
-        />
-      </Head>
       <header>
         <Menu />
       </header>
@@ -83,7 +26,6 @@ const IndexPage = () => {
         <HistoriesBlock />
         <EndBlock />
       </main>
-
       <Footer />
     </>
   );
